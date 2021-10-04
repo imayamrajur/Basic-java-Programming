94c16b69753119c96cdc4db604e73f57cb6a39f0
RAJU M <88897260+imayamrajur@users.noreply.github.com>
2021-10-04 10:32:45 +0530

    Add files via upload

---
+++ ArithSample.java
@@ -0,0 +1,16 @@
+package com.raju.arith;
+
+public class ArithSample {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=3;
+		System.out.println("Add ="+(a+b));
+		System.out.println("Sub ="+(a-b));
+		System.out.println("Multi ="+(a*b));
+		System.out.println("Div ="+(a/b));
+		System.out.println("Model ="+(a%b));
+
+	}
+
+}
